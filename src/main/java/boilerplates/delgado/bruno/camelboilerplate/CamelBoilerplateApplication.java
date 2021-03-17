cbe1f0016fb7df6401e93af0bca49161bf74aa8a
Bruno Delgado <bdelgado@impresa.pt>
2021-03-17 15:43:45 +0000

    Basic dependencies and project boilerplate

---
+++ src/main/java/boilerplates/delgado/bruno/camelboilerplate/CamelBoilerplateApplication.java
@@ -0,0 +1,13 @@
+package boilerplates.delgado.bruno.camelboilerplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelBoilerplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelBoilerplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boilerplates/delgado/bruno/camelboilerplate/CamelBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package boilerplates.delgado.bruno.camelboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
